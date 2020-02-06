f53f8b1b38f958f5ac027e2c3a85417273f98309
Hansin Susatya <hansinsusatya@gmail.com>
2020-02-06 21:58:45 +0700

    Update package.json

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ if (process.env.NODE_ENV == 'development') {
 }
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 const routes = require('./routes')
 
 app.use('/', routes)
